1380273c77f91829e2ce0cbb1ee1d0c92081429c
dovietbinhduong <85276365+dovietbinhduong@users.noreply.github.com>
2021-06-03 13:28:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Đào Thị Vân Anh ơiiii !',
+    introDesc: `Sau đây là vài câu hỏi khảo sát khá là dễ
+    Em làm thử đi nhé :) `,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'Em yêu anh bao nhiêu % rồii 😙',
+    desc: 'Chọn đúng mới đc đó nhaa ',
+    btnYes: 'Em yêu anh 100% luôn <33',
+    btnNo: 'Vẫn 30% thôi em zai à',
+    question:'Hehe trêu em tí thôi h cho trả lời lại này. Em yêu ai nhất nào <3',
+    btnReply: 'Gửi cho ĐVBD <3',
+    reply: 'Em yêu ĐVBD nhất trần đờiii <33333333',
+    mess: 'Anh biết mà 🥰. ANh cũng yêu em nhiều nắmm 😘😘',
+    messDesc: 'Tối nay 10h lên discord choa anh xem mặt nhé bae.',
+    btnAccept: 'Okiiiii lun anh iu <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
